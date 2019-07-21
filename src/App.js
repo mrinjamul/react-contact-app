b777940cd3ba2ea1ac4355daf84441f1e3cb0679
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-21 11:27:57 +0530

    Added Database of contacts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from "react"
 import ContactCard from "./components/ContactCard"
+import contacts from "./components/contactsData"
 
 import "./App.css"
 
 function App() {
+    const Contacts = contacts.map(contact => <ContactCard key={contact.id} name={contact.name} imgUrl={contact.imgUrl} phone={contact.phone} email={contact.email} />)
     return (
         <div className="contacts">
-            <ContactCard
-                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-            />
-
-            <ContactCard
-                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-            />
-
-            <ContactCard
-                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-            />
-
-            <ContactCard
-                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-            />
-
+          {Contacts}
         </div>
     )
 }
